59ec1fe0e5c4b5e5afed9424c2ad7a86d280aedf
PradeepaBharathi <bharathi83201@gmail.com>
2023-06-14 09:46:15 +0530

    second commit

--- src/components/AddonPages/Login.js
+++ src/components/AddonPages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import LogoutModal from '../LogoutModal'
+// import LogoutModal from '../LogoutModal'
 
 const Login = () => {
   return (
--- src/components/TablesPage/Tables.js
+++ src/components/TablesPage/Tables.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
 import ScrollToTop from '../ScrollToTop'
@@ -55,7 +55,7 @@ const Tables = ({ handleOpen,handleOpen1,handleOpen2,open,open1,open2,setOpen,
                 DataTables is a third party plugin that is used to generate the
                 demo table below. For more information about DataTables, please
                 visit the&nbsp;
-                <a target="_blank" href="https://datatables.net">
+                <a target="_blank"  rel="noreferrer" href="https://datatables.net">
                   official DataTables documentation
                 </a>
                 .
